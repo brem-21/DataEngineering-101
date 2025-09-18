ded4b8e84547061c1d4bdaa618315ecc2b5c66b0
Brempong Appiah Dankwah <brempong.dankwah@amalitech.com>
2025-09-18 11:42:14 +0000

    Add function definitions and class examples to Start.ipynb; update script.py for improved readability

--- Python/stock-trading-app/script.py
+++ Python/stock-trading-app/script.py
@@ -3,5 +3,23 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
+limit = 1000
 
 API_KEY = os.getenv('POLYGON_API_KEY')
+
+url = f"https://api.polygon.io/v3/reference/tickers?market=stocks&active=true&order=asc&limit={limit}&sort=ticker&apiKey={API_KEY}"
+
+response = requests.get(url)
+tickers = []
+
+data = response.json()
+for ticker in data['results']:
+    tickers.append(ticker)
+
+while 'next_url' in data:
+    print('requesting next page')
+    url = data['next_url'] + f"&apiKey={API_KEY}"
+    response = requests.get(url)
+    data = response.json()
+    for ticker in data['results']:
+        tickers.append(ticker)
