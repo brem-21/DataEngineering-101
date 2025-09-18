230dc5076d969fb601259b7f9061d46e17df1fcc
Brempong Appiah Dankwah <brempong.dankwah@amalitech.com>
2025-09-18 13:44:37 +0000

    Refactor code structure for improved readability and maintainability

--- Python/stock-trading-app/main.py
+++ Python/stock-trading-app/main.py
@@ -4,7 +4,7 @@ from src.polygon import polygon, save_to_csv
 
 load_dotenv()
 
-limit = 1000
+limit = 100
 API_KEY = os.getenv('POLYGON_API_KEY')
 url = f"https://api.polygon.io/v3/reference/tickers?market=stocks&limit={limit}&apiKey={API_KEY}"
 
@@ -15,16 +15,9 @@ except Exception as e:
     print(f"Connection failed: {e}")
 else:
     data = get_data.fetch_tickets()
-    tickers = get_data.paginate_tickers(data)
-    print(tickers)
-    records = len(tickers)
-    print(f"Total tickers fetched: {records}")
-
-    # Handle pagination if records exceed limit
-    if records > limit and 'next_url' in data:
+    if data:
         get_data.paginate_tickers(data)
-        print(f"Total tickers after pagination: {len(tickers)}")
-        save = save_to_csv(tickers)
+        print("All tickers saved per record to tickers.csv")
 finally:
     print("Connection closed")
 
